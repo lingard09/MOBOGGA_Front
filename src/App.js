78e4483227ecbe6d77476e9b581e55a636901917
lingardd09 <sherlockicker@gmail.com>
2025-04-30 00:20:54 +0900

    create: install react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
+// import { Route, Routes } from "react-router-dom";
+
+// import Header from "./Components/Header";
+// import Footer from "./Components/Footer";
+// import Main from "./Pages/Main";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world
+      {/* <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+      <Footer /> */}
     </div>
   );
 }
